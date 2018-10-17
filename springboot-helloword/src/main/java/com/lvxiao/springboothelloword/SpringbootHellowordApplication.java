7b8e89bfcc0fb895edf91f9978a1befd40570fd4
lvxiao <lvxiao@jd.com>
2018-10-17 21:17:19 +0800

    添加spring-helloword模块

--- springboot-helloword/src/main/java/com/lvxiao/springboothelloword/SpringbootHellowordApplication.java
+++ springboot-helloword/src/main/java/com/lvxiao/springboothelloword/SpringbootHellowordApplication.java
@@ -2,11 +2,19 @@ package com.lvxiao.springboothelloword;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringbootHellowordApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootHellowordApplication.class, args);
+    }
+
+    @RequestMapping("/hello")
+    public String helloWord() {
+        return "helloWord";
     }
 }
