6bd31390755481e4311162ecbd2c2f5e389b3783
lvxiao <lvxiao@jd.com>
2018-10-17 16:58:10 +0800

    提交代码

---
+++ springboot-helloword/src/main/java/com/lvxiao/springboothelloword/SpringbootHellowordApplication.java
@@ -0,0 +1,12 @@
+package com.lvxiao.springboothelloword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHellowordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootHellowordApplication.class, args);
+    }
+}
