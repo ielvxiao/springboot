992bab888ad00614259be9722e7916f9c601ec3c
lvxiao <ielvxiao@gmail.com>
2018-12-23 23:25:43 +0800

    添加日志

--- springboot-web/src/main/java/com/lvxiao/SpringBootStarter.java
+++ springboot-web/src/main/java/com/lvxiao/SpringBootStarter.java
@@ -1,16 +1,7 @@
 package com.lvxiao;
 
-import com.alibaba.fastjson.serializer.SerializerFeature;
-import com.alibaba.fastjson.support.config.FastJsonConfig;
-import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.converter.HttpMessageConverter;
-
-import java.nio.charset.Charset;
 
 /**
  * Author lvxiao
